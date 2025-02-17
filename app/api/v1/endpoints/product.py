f4e945050434d2632c18353d0e29b60a0fefd059
erendonc <erendonc@choucairtesting.com>
2025-02-17 17:20:28 -0500

    Create a authenticated middleware and protected routes

--- app/api/v1/endpoints/product.py
+++ app/api/v1/endpoints/product.py
@@ -2,15 +2,21 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List, Optional, Dict, Type
 
+from app.api.core.security import TokenData
 from app.api.domain.repositories.product_repository import ProductRepository
 from app.api.domain.schemas.product import ProductCreate, ProductUpdate, Product
 from app.api.domain.services.product_service import ProductService
 from app.db.session import get_db
+from app.middleware.auth_middleware import get_current_user
 
 router: APIRouter = APIRouter()
 
 @router.post("/products/", response_model=Product)
-async def create_product(product: ProductCreate, db: Session = Depends(get_db)) -> Product | None:
+async def create_product(
+        product: ProductCreate,
+        db: Session = Depends(get_db),
+        current_user: TokenData = Depends(get_current_user)
+) -> Product | None:
     product_repository: ProductRepository = ProductRepository(db)
     product_service: ProductService = ProductService(product_repository)
     return product_service.create_product(product)
@@ -31,7 +37,12 @@ def read_all_products(db: Session = Depends(get_db)) -> List[Type[Product]]:
     return product_service.get_all_products()
 
 @router.put("/products/{product_id}", response_model=Product)
-def update_product(product_id: int, product: ProductUpdate, db: Session = Depends(get_db)) -> Product:
+def update_product(
+        product_id: int,
+        product: ProductUpdate,
+        db: Session = Depends(get_db),
+        current_user: TokenData = Depends(get_current_user)
+) -> Product:
     product_repository: ProductRepository = ProductRepository(db)
     product_service: ProductService = ProductService(product_repository)
     updated_product: Optional[Product] = product_service.update_product(product_id, product.model_dump())
@@ -40,7 +51,11 @@ def update_product(product_id: int, product: ProductUpdate, db: Session = Depend
     return updated_product
 
 @router.delete("/products/{product_id}")
-def delete_product(product_id: int, db: Session = Depends(get_db)) -> Dict[str, str]:
+def delete_product(
+        product_id: int,
+        db: Session = Depends(get_db),
+        current_user: TokenData = Depends(get_current_user)
+) -> Dict[str, str]:
     product_repository: ProductRepository = ProductRepository(db)
     product_service: ProductService = ProductService(product_repository)
     deleted_product: Optional[Product] = product_service.delete_product(product_id)
---
+++ app/middleware/auth_middleware.py
@@ -0,0 +1,17 @@
+from fastapi import HTTPException, Depends, status
+from fastapi.security import OAuth2PasswordBearer
+
+from app.api.core.security import decode_token
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    token_data = decode_token(token)
+    if token_data is None:
+        raise credentials_exception
+    return token_data
